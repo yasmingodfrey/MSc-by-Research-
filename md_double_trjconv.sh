e800498fdeac86ed41b95f0256da22ad24e56cce
Yasmin Godfrey <yasmin.godfrey27@gmail.com>
2026-03-02 14:59:03 +0000

    Create md_double_trjconv.sh

---
+++ md_double_trjconv.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+dir="134_7"
+
+cd "$dir/gromacs" || {
+    echo "Directory $dir/gromacs not found"
+    exit 1
+}
+        for i in 3; do
+            echo -e "1\n0" | gmx trjconv -f md_${i}.xtc -s md_${i}.tpr -pbc nojump -o md_${i}.noj.xtc -center
+        done
+
+        for i in 3; do
+            echo 0 | gmx trjconv -f md_${i}.noj.xtc -s md_${i}.tpr -pbc mol -o md_${i}.pbc.xtc
+        done
+cd - > /dev/null
